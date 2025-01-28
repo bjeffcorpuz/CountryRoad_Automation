e3507ade0cbd54ab7979dcadc79660fcc0f76625
Jeff <bjeffcorpuz@gmail.com>
2025-01-28 11:59:40 +0800

    click tracking update 2025

--- js/main.js
+++ js/main.js
@@ -1143,14 +1143,14 @@ ${tierLinks.memberTier}
     let deeplinks = `
 {% comment %} Deep link code {% endcomment %}
 <!--
-{% assign clicktracking = 'clicktracking=off ' %}
 
 {% if \${country} == 'Australia' or \${country} == 'AU' %}  
-${tier}
+\t{% assign clicktracking = 'clicktracking=off ' %}${tier}
 ${auLinks}
 {% else %}
 
 {% comment %} Set other countries links {% endcomment %}
+\t{% assign clicktracking = '' %}
 ${nzLinks}
 {% endif %}
 
